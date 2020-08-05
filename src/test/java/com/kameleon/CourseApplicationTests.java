1525e94f7d193af25a40d0a5d62e9b2e0f6ff4de
KameleonHome <kameleonhomepl@gmail.com>
2020-08-05 15:24:53 +0200

    1st commit

---
+++ src/test/java/com/kameleon/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kameleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
